784f6d34cf1b5b24b6a81524fe3794b153f01346
redhawkIT <khodkodmitry10011983@hotmail.com>
2017-04-24 14:29:24 -0700

    Implemented session.go

--- middleware/logrequests.go
+++ middleware/logrequests.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// func logReq(r *http.Request) {
+// 	log.Println(r.Method, r.URL.Path)
+// }
+
+func logReqs(hfn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		start := time.Now()
+		hfn(w, r) //Execute function
+		fmt.Printf("%v\n", time.Since(start))
+	}
+}
+
+func logRequests(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		start := time.Now()
+		handler.ServeHTTP(w, r)
+		fmt.Printf("%v\n", time.Since(start))
+	})
+}
--- middleware/main.go
+++ middleware/main.go
@@ -9,10 +9,24 @@ import (
 func main() {
 	addr := "localhost:4000"
 
-	http.HandleFunc("/v1/hello1", HelloHandler1)
-	http.HandleFunc("/v1/hello2", HelloHandler2)
-	http.HandleFunc("/v1/hello3", HelloHandler3)
+	mux := http.NewServeMux()
+	muxLogged := http.NewServeMux()
+	muxLogged.HandleFunc("/v1/hello1", HelloHandler1)
+	muxLogged.HandleFunc("/v1/hello2", HelloHandler2)
+	mux.HandleFunc("/v1/hello3", HelloHandler3)
+
+	mux.Handle("/v1/", logRequests(muxLogged))
+
+	// http.HandleFunc("/v1/hello1/", logReqs(HelloHandler1))
+	// http.HandleFunc("/v1/hello2/", logReqs(HelloHandler2))
+	// http.HandleFunc("/v1/hello3/", logReqs(HelloHandler3))
+
+	// log.Fata
+
+	logger := log.New(os.Stout, "", log.LstdFlags)
+	mux.Handle("/v1/", logRequests(logger)(muxLogged))
 
 	fmt.Printf("listening at %s...\n", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	// log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
